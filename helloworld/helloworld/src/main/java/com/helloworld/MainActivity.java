9c927f5da17ed7d22f18035ef7c3aebf245d0933
TheNuttyArchitect <ben@techtriumphs.com>
2014-02-28 19:39:34 -0500

    Added in toast for successful login

--- helloworld/helloworld/src/main/java/com/helloworld/MainActivity.java
+++ helloworld/helloworld/src/main/java/com/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Congrats! Your login succeeded.", Toast.LENGTH_LONG).show();
     }
 
 
